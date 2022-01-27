25cbaf36e983165f64b873f11979bc22eff78ef2
evertjr <evert.ju@gmail.com>
2022-01-27 11:54:59 -0300

    feat: unit test

---
+++ src/components/Input/Input.spec.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'jest-styled-components';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../styles/theme';
+import { render, screen } from '@testing-library/react';
+import Input from '.';
+
+describe('Input component', () => {
+  it('should render error message correctly', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Input
+          type="text"
+          name="name"
+          label="Nome"
+          placeholder="Digite seu nome aqui..."
+          hasError
+          errorMessage="Por favor, digite seu nome completo."
+        />
+      </ThemeProvider>,
+    );
+
+    expect(
+      screen.getByText('Por favor, digite seu nome completo.'),
+    ).toBeInTheDocument();
+  });
+  it('should not show error message when not needed', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Input
+          type="text"
+          name="name"
+          label="Nome"
+          placeholder="Digite seu nome aqui..."
+          errorMessage="Por favor, digite seu nome completo."
+        />
+      </ThemeProvider>,
+    );
+
+    expect(
+      screen.queryByText('Por favor, digite seu nome completo.'),
+    ).not.toBeInTheDocument();
+  });
+});
